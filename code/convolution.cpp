25ee58c3540dd0bf293ea68f0c6a37ca8630e424
stekap000 <stekap000@gmail.com>
2024-04-07 23:41:14 +0200

    Initial convolution

--- code/convolution.cpp
+++ code/convolution.cpp
@@ -1,5 +1,32 @@
 #include "convolution.hpp"
 
-namespace sm {
+#include <cstdio>
 
+namespace sm {
+	u8 *convolve_slow(u8 *s1, int n1, u8 *s2, int n2) {
+		int n = n1 + n2 - 1;
+		u8 *new_data = new u8[n];
+
+		printf("Size: %d ", n);
+
+		reverse(s2, n2);
+		for(int i = 0; i < n; ++i) {
+			if(i % 100000 == 0) printf("%d ", i);
+			int j = 0;
+			int k = 0;
+		
+			if(i < n2) {
+				k = n2 - i - 1;
+			}
+			else {
+				j = i - n2;
+			}
+
+			for(; j < n1 && k < n2; ++j, ++k) {
+				new_data[i] += s1[j] * s2[k];
+			}
+		}
+
+		return new_data;
+	}
 }
--- code/convolution.hpp
+++ code/convolution.hpp
@@ -1,8 +1,10 @@
 #ifndef SM_CONVOLUTION_HPP
 #define SM_CONVOLUTION_HPP
 
+#include "util.hpp"
+
 namespace sm {
-	
+	u8 *convolve_slow(u8 *s1, int n1, u8 *s2, int n2);
 }
 
 #endif // SM_CONVOLUTION_HPP
